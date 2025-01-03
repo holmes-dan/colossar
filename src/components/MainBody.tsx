1986321b65e67c213ccbc1d359f9371b92276271
holmes-dan <holmes.dan@proton.me>
2025-01-03 09:40:40 -0600

    Removed dumb text

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -4,9 +4,7 @@ import background from "../assets/caesar.jpg";
 
 const App = () => {
   return (
-    <Body background={background}>
-      <h1>Colossar is good</h1>
-    </Body>
+    <Body background={background} />
   );
 };
 
